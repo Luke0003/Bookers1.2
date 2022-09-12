8e3e855119a30b8f6bfda35631bcc2e57e93be1f
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-12 14:13:15 +0000

    [Add]ユーザーモデルの作成

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,14 @@ class Book < ApplicationRecord
   validates :body, presence: true
 
   has_one_attached :image
+  belongs_to :user
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join("app/assets/images/default-image.jpeg")
+      image.attach(io: File.open(file_path), filename: "default_image", content_type: "image/jpeg")
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+
 end
