a72398891347d3d00d38d69618c8527b37d3fa28
EC2 Default User <ec2-user@ip-172-31-15-106.ap-northeast-1.compute.internal>
2022-09-13 15:04:03 +0000

    [Add]いいね機能の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,6 +5,11 @@ class Book < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 
   def get_image(width, height)
     unless image.attached?
